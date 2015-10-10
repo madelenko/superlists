d6940359b9be421608f08667d84e6d1e5b459ffc
Your Name <jvhernandez445@gmail.com>
2015-10-10 23:01:07 +0100

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -107,7 +107,7 @@ class NewVisitorTest(LiveServerTestCase):
 			delta=5
 		)
 		
-		self.fail('Finish the test')
+
 
 
 
